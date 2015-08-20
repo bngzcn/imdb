255c38a54cf886de2616356d713cb562e4a298e0
Bengü Özcan <bengu.ozcan@bil.omu.edu.tr>
2015-08-20 11:43:58 +0300

    controller, model, view files created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+ root to: "home#index"
+
+ resources :movies do 
+ 	resources :casts
+ end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
